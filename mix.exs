e5f54a3a929a7eddcf07fb21b10da763b39553db
Giorgio Torres <giorgio.torres@sumup.com>
2020-02-04 17:21:33 -0300

    Changed mix.exs with package info.

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule PrettyLoggex.MixProject do
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
-      source_url: "https://github.com/gprimola/pretty_loggex"
+      source_url: "https://github.com/gprimola/pretty_loggex",
+      package: package()
     ]
   end
 
@@ -30,6 +31,18 @@ defmodule PrettyLoggex.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:jason, "~> 1.1"}
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "pretty_loggex",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/gprimola/pretty_loggex"}
     ]
   end
 end
