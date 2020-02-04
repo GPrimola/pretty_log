671cf39cccccd9b104fb1094fbd9f50812f169ec
Giorgio Torres <giorgio.torres@sumup.com>
2020-02-04 17:33:44 -0300

    Added json/4, kv/4 and key_value/4 to PrettyLoggex module.

--- lib/pretty_loggex.ex
+++ lib/pretty_loggex.ex
@@ -1,2 +1,15 @@
 defmodule PrettyLoggex do
+
+  alias PrettyLoggex.Formatters.{JsonFormatter, KvFormatter}
+
+  def json(level, message, timestamp, metadata) do
+    JsonFormatter.format(level, message, timestamp, metadata)
+  end
+
+  def key_value(level, message, timestamp, metadata) do
+    KvFormatter.format(level, message, timestamp, metadata)
+  end
+
+  def kv(level, message, timestamp, metadata),
+    do: key_value(level, message, timestamp, metadata)
 end
