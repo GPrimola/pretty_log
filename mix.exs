89d3e5b132b9fc2852081ccc1ec2ab9164bcf29b
Lucasbot <lucas.primola@gmail.com>
2021-01-18 20:23:25 +0000

    Updated README.md and mix.exs to version .

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PrettyLoggex.MixProject do
   use Mix.Project
 
-  @version "1.0.1"
+  @version "1.0.2"
   @source_url "https://github.com/GPrimola/pretty_loggex"
   @licenses ["Apache-2.0"]
 
