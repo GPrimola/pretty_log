259cfe11450230d296b7999c62b9f840bed04f55
Gio Lucas Torres <torres.giorgio@icloud.com>
2024-03-02 17:22:53 +0100

    Installed dialyzer

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule PrettyLoggex.MixProject do
   use Mix.Project
 
+  @package_name "pretty_loggex"
   @version "1.0.9"
   @source_url "https://github.com/GPrimola/pretty_loggex"
   @licenses ["Apache-2.0"]
@@ -23,6 +24,10 @@ defmodule PrettyLoggex.MixProject do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/pretty_loggex.plt"},
+        list_unused_filters: true
       ]
     ]
   end
@@ -42,15 +47,14 @@ defmodule PrettyLoggex.MixProject do
     [
       {:jason, "~> 1.1"},
       {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.13.2", only: :test}
+      {:excoveralls, "~> 0.13.2", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp package() do
     [
-      # This option is only needed when you don't want to use the OTP application name
-      name: "pretty_loggex",
-      # These are the default files included in the package
+      name: @package_name,
       files: ~w(lib .formatter.exs mix.exs README* VERSION),
       licenses: @licenses,
       links: %{"GitHub" => @source_url}
